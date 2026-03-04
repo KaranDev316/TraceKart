6e548209cd4febcb29a426b8805233fd7a5cffc1
Alfred Mtambalika <karanlicka@gmail.com>
2026-03-04 18:33:36 +0530

    Create card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,23 @@
+import Button from "./Button";
+import image from "./profilepic1.jpeg";
+
 
 function Card(){
     return(
-        <p>Card</p>
+        <div className="border rounded-2xl shadow-xl w-96 border-none bg-[#222f54] ">
+            <img src={image} alt="house keys" className="w-full h-full"/>
+                <div className="flex flex-col items-start p-4">
+                     <h2 className="text-xl font-bold text-white">House Keys</h2>
+                    <p className="text-white">Central Park, near the fountain</p>
+                    <p className="text-white">2/18/2026</p>
+                    <hr />
+                    <p className="text-green-300 my-3">Contact: 12334567</p>
+                </div>
+            <div className="p-4">
+                <Button title="Claim Item" style="bg-[#f0c929] text-white px-4 py-2 rounded-md w-full" />
+            </div>
+            
+        </div>
     )
 }
-export default Card;
+export default Card;    
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,21 @@
+import Card from "../components/Card";
 
 
 function Home(){
     return(
-        <p>Home</p>
+        <div>
+            <div>
+                <h1 className="text-3xl font-bold text-center mt-10">Lost something?</h1>
+                <h1 className="text-2xl text-center mt-4">Report it here and we'll help you find it!</h1>
+                <p className="text-center mt-4">A community-driven platform to help you recover lost items.</p>
+            </div>
+            <div className="border">
+                <h1 className="text-2xl font-bold  mt-10">Recently Found Items</h1>
+               <Card />
+            </div>
+        </div>
+     
+     
     );
 }
 export default Home;
