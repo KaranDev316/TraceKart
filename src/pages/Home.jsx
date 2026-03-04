0bba70e5d244233c8ba03bfef303c4b0d7e856a1
Alfred Mtambalika <karanlicka@gmail.com>
2026-03-04 18:47:16 +0530

    fixing ui issue

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,9 +9,13 @@ function Home(){
                 <h1 className="text-2xl text-center mt-4">Report it here and we'll help you find it!</h1>
                 <p className="text-center mt-4">A community-driven platform to help you recover lost items.</p>
             </div>
-            <div className="border">
-                <h1 className="text-2xl font-bold  mt-10">Recently Found Items</h1>
-               <Card />
+            <div className="border mt-6">
+                <h1 className="text-2xl font-bold  mt-2 flex items-start ml-4">Recently Found Items</h1>
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
+                    <Card />
+                    <Card />
+                    <Card />
+                </div> 
             </div>
         </div>
      
